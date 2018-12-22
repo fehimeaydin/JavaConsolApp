77599018f29acab9b4af6f6247f60fbaa60a68ca
Fehime <Fehime@Fehime.hacettepe.edu.tr>
2018-12-22 07:17:16 +0300

    ödev güncellendi

--- src/faydin/base/ornek9Odev161218/Odev.java
+++ src/faydin/base/ornek9Odev161218/Odev.java
@@ -10,8 +10,8 @@ public class Odev {
 	
 	public Odev() {
 		//javaStringReverse();        // tamam
-		javaSubstringComparisons(); // ilk ve son deðeri döndürmek kaldý
-		//javaSubarray();             // 
+		//javaSubstringComparisons(); // ilk ve son deðeri döndürmek kaldý
+		javaSubarray();             // 
 		//calculator();               // tamam
 		
 		
@@ -98,7 +98,28 @@ public class Odev {
 	
 	private void javaSubarray() {
 		
-		
+		Scanner scan = new Scanner(System.in);
+        int size     = scan.nextInt();
+        int[] array = new int[size];        
+        for (int i = 0; i < size; i++) {
+            array[i] = scan.nextInt();
+        }
+        
+        
+            int count = 0;
+            for (int i = 0; i < array.length; i++) {
+                int sum = 0;
+                for (int j = i; j < array.length; j++) {
+                    sum += array[j];
+                    if (sum < 0) {
+                        count++;
+                    }
+                }
+            }
+        
+        System.out.println(count);
+            
+        scan.close();
 	}
 
 	private void calculator() {
